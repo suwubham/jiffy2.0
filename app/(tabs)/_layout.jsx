dbeb3945cf586a410fae9d37c3cc662d4af0da26
doggydog <dahalankit418@gmail.com>
2024-12-29 11:05:11 +0545

    Add Achievement tab

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -40,6 +40,13 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
+      <Tabs.Screen
+        name="achievement"
+        options={{
+          title: 'Achievement',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+        }}
+      />
     </Tabs>
   );
 }
