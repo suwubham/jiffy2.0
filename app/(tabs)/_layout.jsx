52ade1355a2db8000996b2dcb7583a77a8b88696
Saral Sainju Jodnutech <sainju.saral433@gmail.com>
2024-12-29 14:31:49 +0545

    Navbar edits

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,71 +1,3 @@
-// import { Tabs } from "expo-router";
-// import React from "react";
-// import { Platform } from "react-native";
-
-// import { HapticTab } from "@/components/HapticTab";
-// import { IconSymbol } from "@/components/ui/IconSymbol";
-// import TabBarBackground from "@/components/ui/TabBarBackground";
-// import { Colors } from "@/constants/Colors";
-// import { useColorScheme } from "@/hooks/useColorScheme";
-
-// export default function TabLayout() {
-//   const colorScheme = useColorScheme();
-
-//   return (
-//     <Tabs
-//       screenOptions={{
-//         tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
-//         headerShown: false,
-//         tabBarButton: HapticTab,
-//         tabBarBackground: TabBarBackground,
-//         tabBarStyle: Platform.select({
-//           ios: {
-//             // Use a transparent background on iOS to show the blur effect
-//             position: "absolute",
-//           },
-//           default: {},
-//         }),
-//       }}
-//     >
-//       <Tabs.Screen
-//         name="index"
-//         options={{
-//           title: "Home",
-//           tabBarIcon: ({ color }) => (
-//             <IconSymbol size={28} name="house.fill" color={color} />
-//           ),
-//         }}
-//       />
-//       <Tabs.Screen
-//         name="explore"
-//         options={{
-//           title: "Explore",
-//           tabBarIcon: ({ color }) => (
-//             <IconSymbol size={28} name="paperplane.fill" color={color} />
-//           ),
-//         }}
-//       />
-//       <Tabs.Screen
-//         name="leaderboard"
-//         options={{
-//           title: "Leaderboard",
-//           tabBarIcon: ({ color }) => (
-//             <IconSymbol size={28} name="leaderboard.fill" color={color} />
-//           ),
-//         }}
-//       />
-//       <Tabs.Screen
-//         name="achievement"
-//         options={{
-//           title: "Achievement",
-//           tabBarIcon: ({ color }) => (
-//             <IconSymbol size={28} name="medal.fill" color={color} />
-//           ),
-//         }}
-//       />
-//     </Tabs>
-//   );
-// }
 import { Tabs } from "expo-router";
 import React from "react";
 import { Platform, StyleSheet } from "react-native";
@@ -73,9 +5,21 @@ import { HapticTab } from "@/components/HapticTab";
 import { IconSymbol } from "@/components/ui/IconSymbol";
 import { Colors } from "@/constants/Colors";
 import { useColorScheme } from "@/hooks/useColorScheme";
+import { useFonts } from "expo-font";
+import {
+  Montserrat_500Medium,
+  Montserrat_700Bold,
+  Montserrat_900Black_Italic,
+} from "@expo-google-fonts/montserrat";
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
+
+  const [loaded] = useFonts({
+    Montserrat_700Bold,
+    Montserrat_500Medium,
+    Montserrat_900Black_Italic,
+  });
 
   return (
     <Tabs
--- app/(tabs)/leaderboard.jsx
+++ app/(tabs)/leaderboard.jsx
@@ -5,14 +5,14 @@ import { Ionicons } from '@expo/vector-icons';
 
 const LeaderboardItem = ({ item, index }) => {
   const getBackgroundColor = () => {
-    if (index === 0) return "#FFCA28"; // Gold for 1st place
+    if (index === 0) return "#FFCA28"; 
     if (index === 1) return "#C0C0C0"; // Silver for 2nd place
     if (index === 2) return "#CD7F32"; // Bronze for 3rd place
     return "white"; // Default color for others
   };
   return (
     <View style={[styles.item, { backgroundColor: getBackgroundColor() }]}>
-      <Ionicons name="arrow-up-outline"/>
+      <View style={{padding:15}}><Ionicons name="arrow-up-outline" color="green"/></View>
       <Text style={styles.rank}>{index + 1}</Text>
       <Image source={{ uri: item.avatar }} style={styles.avatar} />
       <View style={styles.userInfo}>
@@ -31,37 +31,36 @@ const LeaderboardDashboard = () => {
       name: "JSa",
       username: "jsa",
       score: 1000,
-      avatar: "https://placekitten.com/100/100",
+      avatar: "https://img.freepik.com/free-photo/young-beautiful-woman-pink-warm-sweater-natural-look-smiling-portrait-isolated-long-hair_285396-896.jpg?semt=ais_hybrid",
     },
     {
       id: "2",
       name: "Janith",
       username: "janith",
       score: 950,
-      avatar: "https://placekitten.com/101/101",
+      avatar: "https://media.istockphoto.com/id/1389348844/photo/studio-shot-of-a-beautiful-young-woman-smiling-while-standing-against-a-grey-background.jpg?s=612x612&w=0&k=20&c=anRTfD_CkOxRdyFtvsiPopOluzKbhBNEQdh4okZImQc=",
     },
     {
       id: "3",
       name: "Bob John",
       username: "bobj",
       score: 900,
-      avatar: "https://placekitten.com/102/102",
+      avatar: "https://i.pinimg.com/originals/13/08/a2/1308a28875d5f2f9fb9fe33a411b9298.jpg",
     },
     {
       id: "4",
       name: "Ali Brown",
       username: "alib",
       score: 850,
-      avatar: "https://placekitten.com/103/103",
+      avatar: "https://t4.ftcdn.net/jpg/02/24/86/95/360_F_224869519_aRaeLneqALfPNBzg0xxMZXghtvBXkfIA.jpg",
     },
     {
       id: "5",
       name: "Ch Davi",
       username: "charls",
       score: 800,
-      avatar: "https://placekitten.com/104/104",
+      avatar: "https://static.vecteezy.com/system/resources/thumbnails/034/332/611/small_2x/ai-generated-studio-portrait-of-handsome-elderly-old-african-man-on-different-colours-background-photo.jpg",
     },
-    // Add more users as needed
   ];
 
   return (
